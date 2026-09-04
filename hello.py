0b7832f323431fb72000cf8e6ee47b0a78a3ebcb
nanditha <2605399@sit.singaporetech.edu.sg>
2026-09-04 15:31:11 +0800

    Fun Conditions

--- hello.py
+++ hello.py
@@ -26,7 +26,7 @@ print ("ZDay 3:", followers)
 
 #Activity 4
 username =input("Enter Username:")
-age = input("Enter Age: ")
+age = int(input("Enter Age: "))
 category = input("Enter Content Category: ")
 
 print("\nInstagram Profile")
@@ -35,3 +35,6 @@ print("username:", username)
 print("Age:", age)
 print("Category:", category)
 
+#Activity 5
+if age>40 and category == "fun":
+    print("You are old what is fun for you??")
