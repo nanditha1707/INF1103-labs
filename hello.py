82ba0544be82676cc680ac30b5a92d359bc5c392
nanditha <2605399@sit.singaporetech.edu.sg>
2026-09-04 15:28:36 +0800

    Assignment Variables

--- hello.py
+++ hello.py
@@ -1,8 +1,10 @@
+#Activity 1
 print("===================================")
 print("Welcome here")
 print("My first post!")
 print("===================================")
 
+#Activity 2
 username ="cool_creator"
 bio = "fun blogger"
 followers = 100
@@ -10,3 +12,14 @@ followers = 100
 print ('Username:', username)
 print ('Bio:', bio)
 print ('Followers:', followers)
+
+#Activity 3
+followers += 50
+print ("ZDay 1:", followers)
+
+followers += 20
+print ("ZDay 2:", followers)
+
+followers -= 10
+print ("ZDay 3:", followers)
+
